f3a45c4ed7f0cc4de19b60b32c99631ac0cb42d1
KANGMINSEO0 <juhee0629032@gmail.com>
2024-02-17 22:08:10 +0900

    initial commit

---
+++ src/main/java/com/ohgiraffers/practice01/Application1.java
@@ -0,0 +1,17 @@
+package com.ohgiraffers.practice01;
+
+public class Application1 {
+
+    public static void main(String[] args) {
+        System.out.println("===================================");
+        System.out.println(" 이 프로그래밍은 Java200이 만들었습니다. ");
+        System.out.println("===================================");
+
+        boolean isL = false;
+        char suit = 'H';
+        int year = 2016;
+        double latitude = 37.52127220511242;
+        double longitude = 127.0074462890625;
+        System.out.println(latitude + "\t" + longitude);  // "\t" : 이스케이프 문자로 탭(7자 띄우기)을 의미함
+    }
+}
