50a1a03560e40cc920b65ba77205483d22f7788a
KANGMINSEO0 <juhee0629032@gmail.com>
2024-03-07 23:52:29 +0900

    to 95-ing commit

--- src/main/java/com/ohgiraffers/practice03/BillboardPrint.java
+++ src/main/java/com/ohgiraffers/practice03/BillboardPrint.java
@@ -14,4 +14,34 @@ public class BillboardPrint {
         } catch (IOException e) {
         }
     }
+    public static void printToJSON(List<Billboard> bblist, String fname) {
+        try(PrintWriter pw = new PrintWriter(new FileWriter(fname, false), true)) {
+            pw.println("{\"billboard\":[");
+            for (int i = 0; i < bblist.size() - 1; i++) {
+                Billboard bb = bblist.get(i);
+                pw.println(pair(bb) + ",");
+            }
+            pw.println(pair(bblist.get(bblist.size() - 1)));
+            pw.println("]}");
+        } catch (IOException e) {
+        }
+    }
+    private static String pair(Billboard bb) {
+        String rank = pair("rank", bb.getRank() + "");
+        String song = pair("song", bb.getSong() + "");
+        String lastweek = pair("Imagesrc", bb.getLastweek() + "");
+        String imagesrc = pair("Imagesrc", bb.getImagesrc() + "");
+        String artist = pair("artist", bb.getArtist() + "");
+        return String.format("{%s,%s,%s,%s,%s}", rank, song, lastweek, imagesrc, artist);
+    }
+    // "key":"value"
+    private static String pair(String key, String value) {
+        String ss = String.format("\"%s\":\"%s\"", key, value);
+        return ss;
+    }
+    // {"key":"value"}
+    private static String pairs(String key, String value) {
+        String ss = String.format("{\"%s\":\"%s\"}", key, value);
+        return ss;
+    }
 }
